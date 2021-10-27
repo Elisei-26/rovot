6fe02674d6667b5c4e88998abeb3000ecdef11bd
Elisei-26 <81551154+Elisei-26@users.noreply.github.com>
2021-10-27 15:35:22 +0100

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,24 +10,29 @@ import Home from './home';
 import Vot from './vot';
 
 const Webpages = () => {
-    return(
-      <div>
-        <Router>
-            <Route exact path="/" component= {Home} />
-            <Route path = "/login" component = {Login} />
-            <Route path = "/register" component = {Register} />
-            <Route path = "/vot" component = {Vot} />
-        </Router>
-        <Router>
-          <Route exact path="/" component= {Vot} />
-          <Route path = "/home" component = {Home} />
-        </Router>
-      </div>
-    );
+  return(
+    <div>
+      <Router>
+        <Route exact path="/" component= {Home} />
+        <Route path = "/login" component = {Login} />
+        <Route path = "/register" component = {Register} />
+        <Route path = "/vot" component = {Vot} />
+        <Route path = "/home" component = {Home} />
+      </Router>
+    </div>
+  );
 };
+
 export default Webpages;
 
-/*Ca sa mai adaugi o pagina:
+/*
+
+<Router>
+        <Route exact path="/" component= {Vot} />
+        <Route path = "/home" component = {Home} />
+      </Router>
+
+Ca sa mai adaugi o pagina:
 
 Creezi o pagina normala in src/pages/
 
